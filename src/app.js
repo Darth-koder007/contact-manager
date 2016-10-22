77f057b23d8bba0ce27515d7d8abdbf23ad9fdc3
Darth-koder007 <vj2010sn@gmail.com>
2016-10-22 23:32:24 +0530

    initial setup complete

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ export class App {
 		config.title = 'Contacts';
 		config.map([
 			{ route: '', moduleId: 'no-selection', title: 'Select' },
-			{ router: 'contacts/:id', moduleId: 'contact-detail', name: 'contacts' }
+			{ route: 'contacts/:id', moduleId: 'contact-detail', name: 'contacts' }
 		]);
 		
 		this.router = router;
